3a15c9f2ef8cc5c95fe1dffcb966bc4ebc1fd052
ashrafmpatel <ashrafmpatel@yahoo.com>
2023-07-26 14:37:21 +0100

    Example commit

--- backup.py
+++ backup.py
@@ -14,17 +14,17 @@ if not os.path.exists(backup_folder_path):
     os.makedirs(backup_folder_path)
 
 
-    def backup_folder(source_folder_path):
-        if not os.path.exists(source_folder_path):
-            log_to_db("BACKUP", "Folder=" + source_folder_path, "ERROR")
-            raise ValueError("Source Folder does not exist")
+def backup_folder(source_folder_path):
+    if not os.path.exists(source_folder_path):
+        log_to_db("BACKUP", "Folder=" + source_folder_path, "ERROR")
+        raise ValueError("Source Folder does not exist")
 
-#Create a timestamp for the backup folder name
-timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
-backup_folder_name = f"{os.path.basename(source_folder_path)}_{timestamp}"
-backup_path = os.path.join(backup_folder_path, backup_folder_name)
+    #Create a timestamp for the backup folder name
+    timestamp = datetime.datetime.now().strftime("%Y%m%d%H%M%S")
+    backup_folder_name = f"{os.path.basename(source_folder_path)}_{timestamp}"
+    backup_path = os.path.join(backup_folder_path, backup_folder_name)
 
-# Copy the contents of the source folder to the backup folder
-shutil.copytree(source_folder_path, backup_path)
+    # Copy the contents of the source folder to the backup folder
+    shutil.copytree(source_folder_path, backup_path)
 
-log_to_db("BACKUP", "Folder=" + source_folder_path, "SUCCESS")
+    log_to_db("BACKUP", "Folder=" + source_folder_path, "SUCCESS")
--- data.py
+++ data.py
@@ -1,8 +1,8 @@
-
-
+import os
+import mysql.connector
 
 from flask import jsonify
-from datevtil import parser
+from dateutil import parser
 
 # Get the backup folder path from environment variable
 sql_host = os.environ.get('SQL_HOST')
@@ -31,80 +31,80 @@ def log_to_db(action, parameter, status):
     db.close()
 
 
-    def get_log(start, end):
-        start_date = None
-        end_date = None
+def get_log(start, end):
+    start_date = None
+    end_date = None
 
-        #check start date is valid
-        if end is not None:
-            start_date = parser.parse(end)
-            start += " 00:00:00"
+    #check start date is valid
+    if end is not None:
+        start_date = parser.parse(end)
+        start += " 00:00:00"
 
-        #check end date is valid
-        if end is not None:
-            end_date = parser.parse(end)
-            end += "23:59:59"
+    #check end date is valid
+    if end is not None:
+        end_date = parser.parse(end)
+        end += "23:59:59"
 
-        if start_date is not None and end_date is not None end_date < start_date:
-            raise ValueError("Start Date is < End Date")
+    if start_date is not None and end_date is not None and end_date < start_date:
+        raise ValueError("Start Date is < End Date")
 
-        db = dbconnect()
-        cursor- = db.cursor()
-        try:
-            select_query = "SELECT date, action, parameter, status FROM log "
+    db = dbconnect()
+    cursor = db.cursor()
+    try:
+        select_query = "SELECT date, action, parameter, status FROM log "
 
-            if start_date is not None and end_date is not None:
-                select_query += "WHERE date >= '" + start + "' AND date <= '" + end + "' "
-            elif start_date is not None and end_date is None:
-                select_query += "WHERE date >= '" + start "' "
-            elif start_date is None and end_date is not None:
-                select_query += "WHERE date <= '" + end + "' "
+        if start_date is not None and end_date is not None:
+            select_query += "WHERE date >= '" + start + "' AND date <= '" + end + "' "
+        elif start_date is not None and end_date is None:
+            select_query += "WHERE date >= '" + start + "' "
+        elif start_date is None and end_date is not None:
+            select_query += "WHERE date <= '" + end + "' "
 
-            select_query += "ORDER BY date"
-            cursor.execute(select_query)
+        select_query += "ORDER BY date"
+        cursor.execute(select_query)
 
-            # Get column names, need this so we can add the keys for the JSON
-            columns = [col[0] for col in cursor.description]
+        # Get column names, need this so we can add the keys for the JSON
+        columns = [col[0] for col in cursor.description]
 
-            # Fetch all rows from the recordset
-            rows = cursor.fetchall()
+        # Fetch all rows from the recordset
+        rows = cursor.fetchall()
 
-            # Convert each row into a dictionary with column names as keys
-            logs = []
-            for row in rows:
-                record = dict(zip(columns, row))
-                logs.append(record)
-        finally:
-            cursor.close()
-            db.close()
+        # Convert each row into a dictionary with column names as keys
+        logs = []
+        for row in rows:
+            record = dict(zip(columns, row))
+            logs.append(record)
+    finally:
+        cursor.close()
+        db.close()
 
-        log_to_db("GET LOGS", "", "SUCCESS")
-        return jsonify(logs)
+    log_to_db("GET LOGS", "", "SUCCESS")
+    return jsonify(logs)
 
 
-    def get_stat():
-        db = dbconnect()
-        cursor = db.cursor()
-        try:
-            backup_count_query = "SELECT COUNT(*) FROM log WHERE action='BACKUP'"
-            cursor.execute(backup_count_query)
-            number_of_backups = cursor.fetchone()
+def get_stat():
+    db = dbconnect()
+    cursor = db.cursor()
+    try:
+        backup_count_query = "SELECT COUNT(*) FROM log WHERE action='BACKUP'"
+        cursor.execute(backup_count_query)
+        number_of_backups = cursor.fetchone()
 
-            success_count_query = "SELECT COUNT(*) FROM log WHERE status='SUCCESS' AND action='BACKUP'"
-            cursor.execute(success_count_query)
-            number_of_successes = cursor.fetchone()
+        success_count_query = "SELECT COUNT(*) FROM log WHERE status='SUCCESS' AND action='BACKUP'"
+        cursor.execute(success_count_query)
+        number_of_successes = cursor.fetchone()
 
-            error_count_query = "SELECT COUNT(*) FROM log WHERE status='ERROR' AND action='BACKUP'"
-            cursor.execute(error_count_query)
-            number_of_errors = cursor.fetchone()
-        finally:
-            cursor.close()
-            db.close()
+        error_count_query = "SELECT COUNT(*) FROM log WHERE status='ERROR' AND action='BACKUP'"
+        cursor.execute(error_count_query)
+        number_of_errors = cursor.fetchone()
+    finally:
+        cursor.close()
+        db.close()
 
-        log_to_db("GET STATS", "", "SUCCESS")
-        return jsonify({
-            "number-of-backups": number_of_backups[0],
-            "successful-backups": number_of_successes[0],
-            "failed-backups": number_of_errors[0]
+    log_to_db("GET STATS", "", "SUCCESS")
+    return jsonify({
+        "number-of-backups": number_of_backups[0],
+        "successful-backups": number_of_successes[0],
+        "failed-backups": number_of_errors[0]
     })
-    dictionary
+    
